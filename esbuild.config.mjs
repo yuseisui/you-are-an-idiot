945ac567954849e1d9a27ec9754b19ab6d1ebaab
yuseisui <53548640+yuseisui@users.noreply.github.com>
2022-04-06 18:32:29 +0900

    style: lint esbuild config

--- esbuild.config.mjs
+++ esbuild.config.mjs
@@ -1,6 +1,6 @@
-import { build } from "esbuild";
-import fs from 'fs/promises';
-import path from 'path';
+import fs from 'node:fs/promises';
+import path from 'node:path';
+import {build} from 'esbuild';
 
 const cleanup = async (dirname) => {
 	await fs.rm(dirname, {recursive: true, force: true});
@@ -13,12 +13,17 @@ const copydir = async (src, dest) => {
 	await fs.mkdir(path.resolve(dest), {recursive: true});
 
 	for (const entry of entries) {
+		// eslint-disable-next-line no-await-in-loop
 		const lstat = await fs.lstat(path.resolve(src, entry));
 
 		if (lstat.isDirectory()) {
-			operations.push(copydir(path.resolve(src, entry), path.resolve(dest, entry)));
+			operations.push(
+				copydir(path.resolve(src, entry), path.resolve(dest, entry)),
+			);
 		} else if (lstat.isFile()) {
-			operations.push(fs.copyFile(path.resolve(src, entry), path.resolve(dest, entry)));
+			operations.push(
+				fs.copyFile(path.resolve(src, entry), path.resolve(dest, entry)),
+			);
 		}
 	}
 
@@ -30,9 +35,7 @@ const outbase = path.resolve('built');
 
 /** @type {import('esbuild').BuildOptions} */
 const buildOptions = {
-	entryPoints: [
-		path.resolve(entrybase, 'main/main.ts'),
-	],
+	entryPoints: [path.resolve(entrybase, 'main/main.ts')],
 	outdir: path.resolve(outbase, 'main'),
 	platform: 'node',
 	format: 'cjs',
@@ -41,5 +44,8 @@ const buildOptions = {
 };
 
 await cleanup(outbase);
-await copydir(path.resolve(entrybase, 'renderer'), path.resolve(outbase, 'renderer'));
+await copydir(
+	path.resolve(entrybase, 'renderer'),
+	path.resolve(outbase, 'renderer'),
+);
 await build(buildOptions);
